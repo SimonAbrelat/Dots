deaa280de6c06e0630b14de51b9a431a53facbf3
Simon Abrelat <simon.abrelat@gmail.com>
2019-11-13 22:09:04 -0500

    initial, partial dots

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Reset to home
+cd ~
+# Add repos
+sudo add-apt-repository ppa:mmstick76/alacritty
+sudo add-apt-repository ppa:neovim--ppa/stable
+sudo apt update
+# Install packages
+## apt
+sudo apt install build-essential git curl compton hsetroot feh neovim
+sudo apt install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev
+sudo apt install libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev gcc make
+sudo apt install libxcb-shape0-dev python3-pip
+sudo apt install alacritty
+# polybar
+sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev
+sudo apt install libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev
+sudo apt install libxcb-icccm4-dev libcurl4-openssl-dev
+# pip
+pip3 install pynvim
+# snap
+snap install spotify
+snap install ripgrep --classic
+
+# adds github projects
+mkdir .source
+cd .source
+git clone https://github.com/baskerville/bspwm
+git clone https://github.com/baskerville/sxhkd
+git clone --depth 1 https://github.com/junegunn/fzf.git
+git clone https://github.com/polybar/polybar
+## Installs bspwm
+cd bspwm
+make
+sudo make install
+sudo cp contrib/freedesktop/bspwm.desktop /usr/share/xsessions/
+mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/sxhkd
+cd ../sxhkd
+make
+sudo make install
+cd ../fzf
+./install
+# polybar
+cd ~/.source/polybar
+bash build.sh
+# neovim
+cd ~/.config
+git clone https://github.com/SimonAbrelat/VimConfig
+mv VimConfig nvim
+
+# Moves config files around
+cd ~/.config/dots
+cp bspwmrc ~/.config/bspwm
+cp sxhkdrc ~/.config/sxhkd
+cp -r polybar ~/.config
+cp -r scripts ~/.config
+
+# DONE
+echo "DONE"
+sleep 1
+reboot
