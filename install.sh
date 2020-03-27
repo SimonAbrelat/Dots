e5a707b28727e07fa0a2acb5c9c97c25a1088aa8
Simon Abrelat <simon.abrelat@gmail.com>
2020-03-26 23:13:06 -0400

    removing wm specific features

--- install.sh
+++ install.sh
@@ -8,14 +8,8 @@ sudo apt update
 # Install packages
 ## apt
 sudo apt install build-essential git curl compton hsetroot feh neovim
-sudo apt install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev
-sudo apt install libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev gcc make
-sudo apt install libxcb-shape0-dev python3-pip
+sudo apt install python3-pip
 sudo apt install alacritty rofi fonts-firacode autojump zathura zathura-pdf-poppler
-# polybar
-sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev
-sudo apt install libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev
-sudo apt install libxcb-icccm4-dev libcurl4-openssl-dev
 # pip
 pip3 install pynvim
 # snap
@@ -24,25 +18,9 @@ snap install ripgrep --classic
 # adds github projects
 mkdir .source
 cd .source
-git clone https://github.com/baskerville/bspwm
-git clone https://github.com/baskerville/sxhkd
 git clone --depth 1 https://github.com/junegunn/fzf.git
-git clone https://github.com/polybar/polybar
-## Installs bspwm
-cd bspwm
-make
-sudo make install
-sudo cp contrib/freedesktop/bspwm.desktop /usr/share/xsessions/
-mkdir -p ~/.config/bspwm
-mkdir -p ~/.config/sxhkd
-cd ../sxhkd
-make
-sudo make install
 cd ../fzf
 ./install
-# polybar
-cd ~/.source/polybar
-bash build.sh
 # neovim
 cd ~/.config
 git clone https://github.com/SimonAbrelat/VimConfig
@@ -63,12 +41,7 @@ wget https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/LS_COLORS
 # Moves config files around
 cd ~/config/Dots
 mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/sxhkd
-mkdir -p ~/.config/bspwm
 cp alacritty.yml ~/.config/alacritty
-cp bspwmrc ~/.config/bspwm
-cp sxhkdrc ~/.config/sxhkd
-cp -r polybar ~/.config
 cp -r scripts ~/.config
 cp -r rofi ~/.config
 cp -r zsh ~/.config
