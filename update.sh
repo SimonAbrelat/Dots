06c7e6edc88a364bfd3136e48e27af71b1bfd380
Simon Abrelat <simon.abrelat@gmail.com>
2020-03-27 20:01:32 -0400

    zsh plugins

--- update.sh
+++ update.sh
@@ -10,5 +10,10 @@ cp -r scripts $c
 cp -r rofi $c
 cp -r zsh $c
 cp -r zathura $c
+mkdir -p $c/zsh/plugins
+cd $c/zsh/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/romkatv/gitstatus
+wget https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/LS_COLORS
 
 echo "UPDATED!"
